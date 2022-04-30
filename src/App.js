860f67925b2b051a76db08ef4891778f5fa26fca
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-04-30 16:42:40 +0700

    Timer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Component } from "react";
+
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: props.start,
+    };
+  }
+
+  componentDidMount() {
+    this.addInterval = setInterval(() => this.increase(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.addInterval);
+  }
+  increase() {
+    this.setState((state, props) => ({
+      time: parseInt(state.time) + 1,
+    }));
+  }
+
+  render() {
+    return <div>{this.state.time}</div>;
+  }
+}
 
 function Introduction(props) {
   return (
     <div>
+      <Timer start="0" />
+      <Timer start="5" />
       <h1>Hallo {props.name}</h1>
       <h1>Age {props.age}</h1>
       <h1>Alamat {props.address}</h1>
